2f34ec727579b534b6cae692ebb39bafdf77a678
Gavin Grant <gavingrant@gmail.com>
2021-06-15 17:23:12 -0700

    refactor: add down arrow on landing

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { DownArrow } from '../public/DownArrow'
 import { useSpring, animated } from 'react-spring'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 export default function Home() {
   const props = useSpring({ 
     to: { filter: 'blur(0)' }, 
-    from: { filter: 'blur(1rem)' },
+    from: { filter: 'blur(0.75rem)' },
     config: { duration: 1000 }, 
+  })
+
+  const arrowProps = useSpring({
+    to: { opacity: '1' }, 
+    from: { opacity: '0' },
+    delay: 1500,
+    config: { duration: 1000 },  
   })
 
   return (
@@ -26,13 +34,16 @@ export default function Home() {
           speed={2.5}
           style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <main className='main landing'>
-              <animated.h1 className='title' style={props}>
-                Gavin Grant Consulting
-              </animated.h1>
-              <animated.p className='description' style={props}>
-                Web Design and Development Services
-              </animated.p>
-            </main>
+            <animated.h1 className='title' style={props}>
+              Gavin Grant Consulting
+            </animated.h1>
+            <animated.p className='description' style={props}>
+              Web Design and Development Services
+            </animated.p>
+            <animated.div className='down-arrow' style={arrowProps}>
+              <DownArrow />
+            </animated.div>
+          </main>
         </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={2} style={{ backgroundColor: '#202020' }} />
---
+++ public/DownArrow.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const DownArrow = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" height="48" width="48">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+  </svg>
+)
+
